9b52bee615fcd9c074feaa56fc10ec04ae931fdb
nupaT <trash_jack@mail.ru>
2020-10-27 16:06:10 +0300

    add: solution of the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 
 // You should implement your task here.
 
+const { maxSatisfying } = require("semver");
+
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArr = [];
+
+  if(!Array.isArray(matrix)){
+    return newArr;
+  }
+
+  for(let i = 0; i < matrix.length; i++){
+    if(i % 2 != 0){
+      for(let rev = matrix[i].length - 1; rev >= 0; rev--){
+        newArr.push(matrix[i][rev]);
+      } 
+    }else {
+        for(let j = 0; j < matrix[i].length; j++){
+          newArr.push(matrix[i][j]);
+        }
+    }
+  }
+
+  return newArr;
 }
